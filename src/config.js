3d8e955f6ff093276cb5076d71539f620ad439d5
Manuel Serra <nitroin@hotmail.com>
2016-05-23 17:49:48 +0200

    Changed dotenv load

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.load();
 
 export const MONGODB_URL = process.env.MONGODB_URL;
 export const VIRTUAL_SENSORS_FORMULAS_COLLECTION_NAME = "virtual-sensors-formulas";
