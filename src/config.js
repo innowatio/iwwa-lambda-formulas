3faa1325ed8b9f3c3007107442b50376dfc5b0f2
Manuel Serra <nitroin@hotmail.com>
2016-05-24 10:41:30 +0200

    Removed unused dependencies

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 
-dotenv.load();
+dotenv.config();
 
 export const MONGODB_URL = process.env.MONGODB_URL;
 export const VIRTUAL_SENSORS_FORMULAS_COLLECTION_NAME = "virtual-sensors-formulas";
