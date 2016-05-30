3904f62a96dfaedea13da64a172271a29fd1ae2b
Manuel Serra <nitroin@hotmail.com>
2016-05-30 17:10:32 +0200

    Added replaced event hook

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import router from "kinesis-router";
 
 import pipeline from "./pipeline";
 
-export const handler = router().on("element inserted in collection sensors", pipeline);
+export const handler = router()
+    .on("element inserted in collection sensors", pipeline)
+    .on("element replaced in collection sensors", pipeline);
