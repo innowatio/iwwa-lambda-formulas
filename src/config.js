88b5ba439cb0ac52621ecdb24db1335c8acb9756
Emanuele Busi <emanuele.busi@mondora.com>
2016-06-13 11:20:15 +0200

    Added `SENSOR_AGGREGATES_COLLECTION_NAME` as environment variable e

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const MONGODB_URL = process.env.MONGODB_URL;
-export const SENSOR_AGGREGATES_COLLECTION_NAME = "readings-daily-aggregates";
+export const SENSOR_AGGREGATES_COLLECTION_NAME = process.env.SENSOR_AGGREGATES_COLLECTION_NAME || "readings-daily-aggregates";
 export const VIRTUAL_SENSORS_FORMULAS_COLLECTION_NAME = "virtual-sensors-formulas";
 
 export const KINESIS_STREAM = process.env.KINESIS_STREAM;
