420ec2e224b1da4407c09d797e80014176433bed
Manuel Serra <nitroin@hotmail.com>
2016-11-23 12:51:57 +0100

    Added logs

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const MONGODB_URL = process.env.MONGODB_URL;
+export const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/test";
 export const SENSOR_AGGREGATES_COLLECTION_NAME = process.env.SENSOR_AGGREGATES_COLLECTION_NAME || "readings-daily-aggregates";
 export const VIRTUAL_SENSORS_FORMULAS_COLLECTION_NAME = "virtual-sensors-formulas";
 
--- src/steps/sensor-save-mongodb.js
+++ src/steps/sensor-save-mongodb.js
@@ -1,6 +1,9 @@
+import log from "../services/logger";
 import {upsertVirtualSensor} from "../services/mongodb";
 
+
 export async function upsertSensorFormulas (id, decoratedSensor) {
+    log.info({decoratedSensor});
     await upsertVirtualSensor(id, {
         measurementType: decoratedSensor.measurementType,
         variables: decoratedSensor.variables,
